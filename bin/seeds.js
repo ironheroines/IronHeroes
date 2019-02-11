bff97d6b72cd20ef648fb88fe4c648163abccd60
fbath <phoenixa@gmx.de>
2019-02-11 16:46:31 +0100

    initial commit

---
+++ bin/seeds.js
@@ -0,0 +1,44 @@
+// Seeds file that remove all users and create 2 new users
+
+// To execute this seed, run from the root of the project
+// $ node bin/seeds.js
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+const bcryptSalt = 10;
+
+mongoose
+  .connect('mongodb://localhost/ironheroes', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+let users = [
+  {
+    username: "NotASuperhero",
+    password: bcrypt.hashSync("hero", bcrypt.genSaltSync(bcryptSalt)),
+    email: "test@test.com"
+  }
+]
+
+User.deleteMany()
+.then(() => {
+  return User.create(users)
+})
+.then(usersCreated => {
+  console.log(`${usersCreated.length} users created with the following id:`);
+  console.log(usersCreated.map(u => u._id));
+})
+.then(() => {
+  // Close properly the connection to Mongoose
+  mongoose.disconnect()
+})
+.catch(err => {
+  mongoose.disconnect()
+  throw err
+})
