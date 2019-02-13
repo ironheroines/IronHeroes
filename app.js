d44f7324d81932e5e27532e3a540757a03bc807b
Ksenia Danniker <kseniadanniker@gmail.com>
2019-02-13 15:05:38 +0100

    deploy with database uri

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
-const session    = require("express-session");
+const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-    
+const flash = require('connect-flash');
 
 mongoose
-  .connect('mongodb://localhost/ironheroes', {useNewUrlParser: true})
+  .connect(
+    'mongodb://heroku_jp94432s:4u5h42bon057fegrjgju6sv464@ds149754.mlab.com:49754/heroku_jp94432s',
+    { useNewUrlParser: true }
+  )
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
+);
 
 const app = express();
 
@@ -36,68 +42,66 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
+  })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
 hbs.registerHelper('ifUndefined', (value, options) => {
   if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
+    throw new Error('Handlebars Helper ifUndefined needs 1 parameter');
+  if (typeof value !== undefined) {
+    return options.inverse(this);
   } else {
-      return options.fn(this);
+    return options.fn(this);
   }
 });
-  
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
 // Enable authentication using session + passport
-app.use(session({
-  secret: 'irongenerator',
-  cookie: { maxAge: 60000 },
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
+app.use(
+  session({
+    secret: 'irongenerator',
+    cookie: { maxAge: 60000 },
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
 app.use(flash());
 require('./passport')(app);
 
 // This middleware gives the variable "isConnected" to the view
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   console.log('Hello everyone!!');
   // This code is executed at every request
   // It defines a view variable isConnectedAndActive = true or false
-  res.locals.isConnectedAndActive = req.user && req.user.status === 'Active'
-  next()
-})
+  res.locals.isConnectedAndActive = req.user && req.user.status === 'Active';
+  next();
+});
 // app.use((req,res, next) => {
 //   res.locals.isConnected = !!req.user
 //   if (req.user) {
 //     res.locals.currentUserId = req.user._id
 //     res.locals.username = req.user.username
 //   }
-//   next() 
+//   next()
 // })
-    
 
 const index = require('./routes/index');
 app.use('/', index);
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
-      
 
 module.exports = app;
