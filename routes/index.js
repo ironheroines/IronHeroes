3529937fe8c661f00a3ac5c0ef430370d1701981
fbath <phoenixa@gmx.de>
2019-02-15 11:31:20 +0100

    homepage styling

--- routes/index.js
+++ routes/index.js
@@ -91,34 +91,5 @@ router.get('/users/:id/delete', (req, res, next) => {
     })
 });
 
-// //POST new request to the database
-// router.post('/new-goal', isLoggedIn, (req,res,next)=> {
-//   Goal.create({
-//     title: req.body.title,
-//     frequency: req.body.frequency,
-//     history: [{date: tools.currentDate(), value: 0}],
-//     _user: req.user._id,
-//     lastUpdate: tools.currentDate(),
-//     nextWeekUpdate: tools.startDayOfFollowingWeek()
-//   })
-//   .then(goal=> {
-//     res.redirect('/')
-//   })
-//   .catch(err=> {
-//     console.log("Error at POST /new-goal", err);
-//   })
-// })
-
-// comment sources
-// var comment = new Comment({
-//   username: req.body.username,
-//   content: req.body.comment,
-//   created: Date.now(),
-// });
-
-// Save a comment in the database
-// comment.save()
-//   .then(comment => {console.log(“The comment was created”)})
-//   .catch(err => { console.log(“An error occured”, err)});
 
 module.exports = router;
